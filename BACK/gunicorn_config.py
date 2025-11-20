4185a6676eca84208779db17896927e921ee1331
wessam elden <wessamelden7@gmail.com>
2025-11-21 01:01:29 +0200

    update data base

--- BACK/gunicorn_config.py
+++ BACK/gunicorn_config.py
@@ -3,7 +3,8 @@ import os
 workers = int(os.environ.get('GUNICORN_WORKERS', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+port = os.environ.get('PORT', '8080')
+bind = os.environ.get('GUNICORN_BIND', f'0.0.0.0:{port}')
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
